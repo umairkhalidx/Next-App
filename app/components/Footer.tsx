2702ad89ae3cdb39ade4f25443e3c3bcfc98f759
Umair Khalid <umair32khalid@gmail.com>
2026-02-09 10:19:43 +0500

    Added Header in Homepage

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="footer" id="footer-section">
+            <p>Footer Content</p>
+        </footer>
+    );
+}
